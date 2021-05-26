2914cef7b64daa5a8b2ea3847f6b2f82fe88af59
Samuel Trégouët <samuel.tregouet@gmail.com>
2021-05-26 17:53:06 +0200

    db: merge migrations
    sqlite doe not allow to add constraint table
    with `alter table` statement, but we need to
    add unique constraint on mail(uid, mailbox).
    For now it is ok to just merge migrations

--- database/migrations/v20210426.go
+++ database/migrations/v20210426.go
@@ -9,14 +9,31 @@ func init() {
 		Version:     "20210426",
 		Description: "initial schema",
 		Statements: []string{
+			`CREATE TABLE account (
+				id INTEGER PRIMARY KEY,
+				name TEXT UNIQUE
+			)`,
+			`CREATE TABLE mailbox (
+				id INTEGER PRIMARY KEY,
+				name TEXT,
+				shortname TEXT,
+				parent TEXT,
+				account INTEGER NOT NULL REFERENCES account(id) ON DELETE CASCADE,
+				UNIQUE (name, account)
+			)`,
+			"CREATE INDEX mailbox_parent_idx ON mailbox(parent)",
 			`CREATE TABLE mail (
 				id INTEGER PRIMARY KEY,
 				threadid INTEGER,
 				date datetime,
 				uid INTEGER,
+				flags TEXT,
 				subject TEXT,
 				messageid TEXT,
-				inreplyto TEXT
+				inreplyto TEXT,
+				mailbox INTEGER NOT NULL REFERENCES mailbox(id) ON DELETE CASCADE,
+				account INTEGER NOT NULL REFERENCES account(id) ON DELETE CASCADE,
+				UNIQUE (uid, mailbox)
 			)`,
 			"CREATE INDEX subject_idx ON mail(subject)",
 			"CREATE INDEX messageid_idx ON mail(messageid)",
